a5af34a288c9f3b2f958952ae07228b988f588fc
Alex <a.chisar00@gmail.com>
2021-06-08 13:52:20 -0400

    dotenv added

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-  
-    password: '',
-    database: 'employeeDB',
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
   });
 
-  connection.connect()
   
-//   connection.connect((err) => {
-//     if (err) throw err;
-    
-//   });
+  connection.connect((err) => {
+    if (err) throw err;
+    start();
+  });
+
+  const start = () => {
+      console.log("Employee Tracker")
+  }
